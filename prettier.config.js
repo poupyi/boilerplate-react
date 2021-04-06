99916919b1df36aae2d61eba68a72b610094949f
Pascal <pascal.yi@affluences.com>
2021-04-07 01:29:18 +0200

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: false,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">aaaaaaaaaa</div>;
+}
+
+export default App;
